7e3f011c8e92c2812cac715dc08abffdbc64ffc2
Charlemagne Wong <frian13@hotmail.com>
2019-02-24 01:50:17 -0600

    Test changes

--- HousWarm.cpp
+++ HousWarm.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <errno.h>
 
 #include <wiringPi.h>
+#include <wiringSerial.h>
 
 int main()
 {
+    int fd;
+    int count;
+
+    fd = serialOpen("/dev/ttyAMA0", 9600);
+    wiringPiSetup();
+
+    std::string str;
+    
+    std::cin >> str;
+    
+    serialPuts(fd, str);
 
     return 0;
 }
